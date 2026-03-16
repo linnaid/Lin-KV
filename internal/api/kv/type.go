6b89fd0e67ee7b18ba989bcaeab005df0d8eec92
linnaid <jiaguoyangyang@qq.com>
2026-03-16 22:20:00 +0800

    add api

--- internal/api/kv/type.go
+++ internal/api/kv/type.go
@@ -1,22 +1,12 @@
 // 结构体类型
 package kv
 
+import "etcd-KV/internal/storage/mvcc"
+
 type PutRequest struct {
-	Key []byte
+	Key string
 	Value []byte
-	LeaseID int64
-	ClientID int64
-	Seq int64
-}
 
-type GetRequest struct {
-	Key []byte
-	ClientID int64
-	Seq int64
-}
-
-type DeleteRequest struct {
-	Key []byte
 	LeaseID int64
 	ClientID int64
 	Seq int64
@@ -28,6 +18,14 @@ type PutResponse struct {
 	Err string
 }
 
+type GetRequest struct {
+	Key string
+	Revision int64
+	
+	ClientID int64
+	Seq int64
+}
+
 type GetResponse struct {
 	Value []byte
 	Revision int64
@@ -36,9 +34,28 @@ type GetResponse struct {
 	Err string
 }
 
+type DeleteRequest struct {
+	Key string
+
+	LeaseID int64
+	ClientID int64
+	Seq int64
+}
+
 type DeleteResponse struct {
 	Revision int64
 	Deleted bool
 
 	Err string
+}
+
+type TxnRequest struct {
+	Txn mvcc.Txn
+
+}
+
+type TxnResponse struct {
+	Succeeded bool
+	kvs []mvcc.KeyValue
+
 }
